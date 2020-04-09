ba5c02b7bcccac284fa1756b955b373503d1c6a5
André Vital <santiago_vital@outlook.com>
2020-04-08 19:26:25 -0500

    Cleaned code. Last commit on likes function

--- main.py
+++ main.py
@@ -43,46 +43,18 @@ class IgBot:
         hrefs = driver.find_elements_by_tag_name('a')
         pic_hrefs = [elem.get_attribute('href') for elem in hrefs]
         pic_hrefs = [href for href in pic_hrefs if '/p/B-' in href]
-        print(hashtag + ' photos: ' + str(len(pic_hrefs)))
-
-        new_Liked = 0
-        old_Liked = 0
-        own_Pics = 0
 
         for idx, pic_href in enumerate(pic_hrefs):
-            print('Photo ' + str(idx))
             driver.get(pic_href)
-            # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
             try:
-                # Like button approach (problem when pic is already liked)
-                # # Fixed it by verifying if like button was active
-                # # # Added filter to exclude own photos
-
                 if len(driver.find_elements_by_css_selector('._97aPb + div > section > span > button.wpO6b > svg[aria-label="Me gusta"]')) >= 1:
-                    # # # print('Not liked yet')
                     if len(driver.find_elements_by_xpath("//header//a[(text() = 'im_andrevital')]")) <= 0:
-                        # # # print('Not your photo')
                         driver.find_element_by_class_name("wpO6b").click()
-                        # # # new_Liked += 1
-                        # # # print('Just liked it! Total likes: ' + str(new_Liked))
                         time.sleep(18)
-                    else: 
-                        # # # own_Pics += 1
-                        # # # print('Your pic / your comment! Count: ' + str(own_Pics))
-                        time.sleep(2)
-                else: 
-                    # # # old_Liked += 1
-                    # # # print('Already liked! Count: ' + str(old_Liked))
-                    time.sleep(2)                
-
-                # Double tap picture approach (Didn't work) (Couldn't emulate double tap)
-                # picture = driver.find_element_by_xpath("//header/following-sibling::div/div[@class='ZyFrc'][@role='button']") 
-                # picture.click()
-                # picture.click()
-            except Exception as e:
-                print("Didn't do shit")
-                time.sleep(2)
+                    else: time.sleep(2)
+                else: time.sleep(2)
+            except Exception as e: time.sleep(2)
 
 myBot = IgBot("username", "password")
 myBot.login();
